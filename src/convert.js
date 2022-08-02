e91f2edd2390edd18dca53211083307bffed9d5a
magnomct <carlos.sycom@gmail.com>
2022-08-02 01:55:13 -0300

    Iniciativa devops 0822
    Iniciativa devops 0822

--- src/convert.js
+++ src/convert.js
@@ -1,7 +1,16 @@
+<<<<<<< HEAD
 exports.celsiusFahrenheit = (valor) => {
     return (valor * 9 / 5) + 32;
 };
 
 exports.fahrenheitCelsius = (valor) => {
     return (valor - 32) * 5 / 9;
+=======
+exports.celsiusFahrenheit = (valor) => {
+    return ((valor * 9) / 5) + 32
+};
+
+exports.fahrenheitCelsius = (valor) => {
+    return ((valor - 32) * 5) / 9;
+>>>>>>> 1fdb122c236118bfb1698ced836895b7fa103847
 };
